b5c655552fce0c44af3904575f8b734d825e3e09
Aydar N. (Ocean-1) <me@aydar.media>
2024-01-20 00:56:37 +0300

    Added chances!

--- index.php
+++ index.php
@@ -1,10 +1,53 @@
 <?php
 
-$dir = '/var/www/html/snootberg/collection'; // replace with your directory
-$images = glob("$dir/{*.jpg,*.png,*.gif}", GLOB_BRACE); // find all images
-$image = array_rand($images); // select random image
+$collection = '/var/www/html/snootberg/collection';
+$formats = '*.jpg,*.png,*.gif';
+$priorities = array(
+    'fox1.jpg' => 30,
+);
 
-header("Content-Type: image/jpeg");
-readfile($images[$image]);
+// ####################
 
-?>
+function globalize($fileName, $fullPath) {
+    if($fileName[0] != '/') {
+        return "$fullPath/$fileName";
+    }
+
+    return $fileName;
+}
+
+function getWeightedRandomSample($weights, $arr) {
+    $totalWeightDefined = array_sum($weights);
+    $undefinedWeight = ($totalWeightDefined <= 100) ? (100 - $totalWeightDefined) / (count($arr) - count($weights)) : 0;
+    
+    foreach($arr as $i) {
+        if(!isset($weights[$i])) $weights[$i] = $undefinedWeight;
+    }
+
+    $n = mt_rand(0, 99);
+
+    $cumulativeWeight = 0;
+    foreach($weights as $i => $weight) {
+        $cumulativeWeight += $weight;
+        if($n < $cumulativeWeight) return $i;
+    }
+
+    // Fallback
+    return $arr[array_rand($arr)];
+}
+
+$isCli = PHP_SAPI === 'cli';
+$images = glob("$collection/{{$formats}}", GLOB_BRACE);
+
+$image = globalize(
+    getWeightedRandomSample($priorities, $images),
+    $collection
+);
+
+if ($isCli) {
+    echo("Chosen $image\n");
+    exit(0);
+}
+
+header('Content-Type: image/jpeg');
+readfile($image);
